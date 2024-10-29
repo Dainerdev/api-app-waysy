00b9e04a5b0c44936c0ff65d8d4061bb9a9e8170
Your Name <you@example.com>
2024-10-29 01:18:52 -0500

    Creacion de la ruta usuario, prueba de la ruta y controlador

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+
+// Routes imports
+import userRoutes from "./routes/user.routes";
+
+const app = express();
+
+// Settings
+app.set("port", 3312);
+
+// Middlewares
+app.use(morgan("dev")); // Modulo morgan modo desarrollo
+app.use(express.json()); 
+
+// Routes
+app.use("/api/users",userRoutes);
+
+export default app;
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+
+const getUsers = (req, res) => {
+    res.json("Soy el controlador")
+};
+
+export const methods = {
+    getUsers
+};

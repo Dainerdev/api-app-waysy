544e550be0e1e4f61e0ba88ed9f6a7039b269ccc
Your Name <you@example.com>
2024-10-29 23:59:03 -0500

    Se crea la guncion getUsers en el controlador para obtener los usuarios, se testea la funcion addUser

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.set("port", 3312);
 
 // Middlewares
 app.use(morgan("dev")); // Modulo morgan modo desarrollo
-app.use(express.json()); 
+app.use(express.json()); // Interpretar json
 
 // Routes
 app.use("/api/users",userRoutes);
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,34 @@
+import { getConnection } from "../database/database";
 
-const getUsers = (req, res) => {
-    res.json("Soy el controlador")
+// Get function
+const getUsers = async (req, res) => {
+    
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT * FROM usuarios");
+        console.log(result);
+        res.json(result);
+    } catch(error) {
+        res.status(500);
+        res.send(error.message);
+    }  
+};
+
+const addUser = async (req, res) => {
+
+    try {
+        console.log(req.body);
+
+        const connection = await getConnection();
+        
+        res.json("addUser")
+    } catch(error) {
+        res.status(500);
+        res.send(error.message);
+    }
 };
 
 export const methods = {
-    getUsers
+    getUsers,
+    addUser
 };

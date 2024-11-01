a4b1cd0db52d737ebee0637c591b5e3808688035
Dainerdev <florianarroyo15@gmail.com>
2024-10-31 22:39:51 -0500

    Se agregan las rutas para cada funcion por separado

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -4,5 +4,9 @@ import { methods as categoryController } from "../controllers/category.controlle
 const router = Router();
 
 router.get("/", categoryController.getCategories); // Find all
+router.get("/:id", categoryController.getCategoryById); // Find by id
+router.post("/", categoryController.addCategory); // Add
+router.put("/:id", categoryController.updateCategory); // Edit
+router.delete("/:id", categoryController.deleteCategory); // Delete
 
 export default router;

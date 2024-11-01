ab6e5a41054cea8a620b9aa355eea9227a965ec0
Dainerdev <florianarroyo15@gmail.com>
2024-11-01 17:58:07 -0500

    Se establecen las rutas para hacer uso de las funciones que obtinen los valores del enum de la BD

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,9 @@ import { methods as userController } from "../controllers/user.controller";
 const router = Router();
 
 router.get("/", userController.getUsers); // Find all
+router.get("/typeId", userController.getTypeId); // Find enum TypeId 
+router.get("/gender", userController.getGender); // Find enum Gender
+router.get("/role", userController.getRole); // Find enum Role
 router.get("/:id", userController.getUserById); // Find by id
 router.post("/", userController.addUser); // Add
 router.put("/:id", userController.updateUser); // Edit

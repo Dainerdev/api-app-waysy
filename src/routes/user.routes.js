3ccee3bd655c4d6cc77d1bf521e7b49825654a7f
Dainerdev <florianarroyo15@gmail.com>
2024-11-01 19:25:47 -0500

    Se agrega la ruta para el uso de la funcion login, se clasifican las rutas para mejor lectura

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,12 +4,18 @@ import { methods as userController } from "../controllers/user.controller";
 
 const router = Router();
 
+// GET
 router.get("/", userController.getUsers); // Find all
 router.get("/typeId", userController.getTypeId); // Find enum TypeId 
 router.get("/gender", userController.getGender); // Find enum Gender
 router.get("/role", userController.getRole); // Find enum Role
 router.get("/:id", userController.getUserById); // Find by id
+
+// POST
 router.post("/", userController.addUser); // Add
+router.post("/login", userController.login); // Validate login
+
+// PUT & DELETE
 router.put("/:id", userController.updateUser); // Edit
 router.delete("/:id", userController.deleteUser); // Delete
 

982b6fb9eb565f581bcaeb5d27a19f83422187ed
Dainerdev <florianarroyo15@gmail.com>
2024-11-05 18:55:09 -0500

    Se agregan las rutas para obtener mediante nombre, fecha y descripcion

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -5,6 +5,11 @@ const router = Router();
 
 router.get("/", categoryController.getCategories); // Find all
 router.get("/:id", categoryController.getCategoryById); // Find by id
+router.get("/id/max", categoryController.getMaxId); // Get Max Id
+router.get("/name/:nombre_categoria", categoryController.getCategoryByName); // Find by Name
+router.get("/date/:fecha_recepcion", categoryController.getCategoryByDate); // Find by Date
+router.get("/description/:descripcion", categoryController.getCategoryByDescription); // Find by Description
+
 router.post("/", categoryController.addCategory); // Add
 router.put("/:id", categoryController.updateCategory); // Edit
 router.delete("/:id", categoryController.deleteCategory); // Delete

e641d9b647605fc27257d5e1a27b5be2b462837a
Dainerdev <florianarroyo15@gmail.com>
2024-11-04 21:10:41 -0500

    Se agregan las rutas para usar las funciones buscar por fecha y descripcion

--- src/routes/source.routes.js
+++ src/routes/source.routes.js
@@ -6,6 +6,8 @@ const router = Router();
 router.get("/", sourceController.getSources); // Find all
 router.get("/:id", sourceController.getSourceById); // Find by id
 router.get("/name/:nombre_fuente", sourceController.getSourceByName); // Find by Name
+router.get("/description/:descripcion", sourceController.getSourceByDescription); // Find by Description
+router.get("/date/:fecha_recepcion", sourceController.getSourceByDate); // Find by Date
 
 router.post("/", sourceController.addSource); // Add
 router.put("/:id", sourceController.updateSource); // Edit

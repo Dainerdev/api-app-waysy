83ec94d7d4be92dc61e0915e8bed2f6cd521b85b
Dainerdev <florianarroyo15@gmail.com>
2024-11-05 18:11:10 -0500

    Se agrega la ruta para el uso de la funcion obener maximo id

--- src/routes/source.routes.js
+++ src/routes/source.routes.js
@@ -8,6 +8,7 @@ router.get("/:id", sourceController.getSourceById); // Find by id
 router.get("/name/:nombre_fuente", sourceController.getSourceByName); // Find by Name
 router.get("/description/:descripcion", sourceController.getSourceByDescription); // Find by Description
 router.get("/date/:fecha_recepcion", sourceController.getSourceByDate); // Find by Date
+router.get("/id/max", sourceController.getMaxId); // Get Max Id
 
 router.post("/", sourceController.addSource); // Add
 router.put("/:id", sourceController.updateSource); // Edit
